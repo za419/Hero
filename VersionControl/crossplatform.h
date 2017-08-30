a1873f361fae3c073dfc28062cb92b72238afd75
Ryan Hodin <ryan951357@gmail.com>
2017-08-30 03:47:58 -0700

    Correct and comment mkdir shim

--- VersionControl/crossplatform.h
+++ VersionControl/crossplatform.h
@@ -1,12 +1,15 @@
 // Crossplatform.h: Simple wrappers to increase cross-platform compatibility
 
 #ifndef CROSSPLATFORM_H
-#include <direct.h>
 #include <cstdlib>
 
+// First, a shim for mkdir
 #if defined(_WIN32)
+#include <direct.h>
 int mkdir(const char* dirname) {
 	return _mkdir(dirname);
 }
+#else
+#include <sys/stat.h>
 #endif
 #endif // !CROSSPLATFORM_H
