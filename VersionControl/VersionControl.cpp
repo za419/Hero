4f28b94fb2fe9d076fd648e59f4b8b07658b2eb6
Ryan Hodin <ryan951357@gmail.com>
2017-08-29 23:45:18 -0700

    Add basic usage method
    Invoking the program now does something, sometimes.

--- VersionControl/VersionControl.cpp
+++ VersionControl/VersionControl.cpp
@@ -3,8 +3,27 @@
 
 #include "../PicoSHA2/picosha2.h"
 
-int main(int argc, char* argv[])
-{
+#include <iostream>
+#include <cstdint>
+#include <cstdlib>
+
+// Internal codes for commands which we know how to handle, plus an error code (unknownCommand)
+enum class Command : uint8_t {unknownCommand, init, add, commit};
+
+void usage(char* invoke, Command source) {
+	if (source == Command::unknownCommand) {
+		std::cout << "Usage:\n";
+		std::cout << invoke << " init\n";
+		std::cout << invoke << " add [files]\n";
+		std::cout << invoke << " commit [files] [-a]\n";
+	}
+	exit(0);
+}
+
+int main(int argc, char* argv[]) {
+	if (argc < 2) {
+		usage(argv[0], Command::unknownCommand);
+	}
     return 0;
 }
 
