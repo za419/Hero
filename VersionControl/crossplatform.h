5a4e1020945826f5c67b674734737ee32893eea7
Ryan Hodin <ryan951357@gmail.com>
2017-08-31 02:16:02 -0700

    Write a shim for file copying

--- VersionControl/crossplatform.h
+++ VersionControl/crossplatform.h
@@ -10,4 +10,32 @@
 #else
 #include <sys/stat.h>
 #endif
+
+// Next, a function to copy files
+// Returns whether the operation succeeded
+#if defined(_WIN32)
+#include <Windows.h>
+#else
+#include <sys/sendfile.h>
+#include <fnctl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#endif
+bool copyfile(const char* source, const char* dest) {
+#if defined(_WIN32)
+	return CopyFile(source, dest, false);
+#else // I'm not sure if this will work. I think it will, but...
+	int src = open(source, O_RDONLY, 0);
+	int dest = open(dest, O_WRONLY | O_CREAT | O_TRUNC);
+
+	struct stat stat_source;
+	fstat(src, &stat_source);
+
+	bool out = sendfile(dest, src, 0, stat_source.st_size);
+
+	close(src);
+	close(dest);
+#endif
+}
 #endif // !CROSSPLATFORM_H
