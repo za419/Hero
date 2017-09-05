37ea699e7c84e9e9c3a1d32fa1dadaa725a8e8b3
Ryan Hodin <ryan951357@gmail.com>
2017-09-05 00:44:54 -0700

    Add escaped function
    Which replaces all occurrences of a term in a given string with a replacement.
    
    This will be used during commit to replace "&" with "&amp;", and later when reading commits to undo that replacement.

--- VersionControl/Utils.h
+++ VersionControl/Utils.h
@@ -3,4 +3,17 @@
 #ifndef UTILS_H
 #define UTILS_H
 #pragma once
+
+#include <string>
+
+std::string escaped(std::string source, const std::string& term, const std::string& replacement) {
+	size_t i(0);
+	size_t termLength(term.length());
+	size_t replacementLength(replacement.length());
+	while ((i = source.find(term, i)) != std::string::npos) {
+		source.replace(i, termLength, replacement);
+		i += replacementLength;
+	}
+	return source;
+}
 #endif
