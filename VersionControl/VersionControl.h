20847dd86fed7f9708575c3ecbf7a90c966a5d43
Ryan Hodin <ryan951357@gmail.com>
2017-09-10 21:06:55 -0700

    Add VersionControl.h
    Which will handle project-specific utility functions, unlike Utils, which contains functions that may fit in other projects.
    
    The current example is getHeadHash, which shall do the leg work of getting the hash of the current head commit. This will especially facilitate the task of incorporating branch support into all commands in 0.04, since all current commands won't notice the existence of branches after its abstracted into this helper.

---
+++ VersionControl/VersionControl.h
@@ -0,0 +1,7 @@
+// VersionControl.h: Utility functions shared across different parts of the program
+// Unlike Utils.h, this file is strongly linked to this project: While Utils might be suitable to be copied elsewhere, this is not.
+
+#ifndef VERSIONCONTROL_H
+#define VERSIONCONTROL_H
+#pragma once
+#endif
