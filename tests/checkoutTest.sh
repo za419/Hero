3185f3e4157fd7cdeb601477902336f9eee51e35
Ryan Hodin <ryan951357@gmail.com>
2017-09-18 23:47:21 -0500

    Add checkout test script

---
+++ tests/checkoutTest.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Tests the checkout function
+
+../x64/Debug/VersionControl.exe init
+git checkout eb2cef163bffc4f3f38663843e5a35a0a4f5744a
+../x64/Debug/VersionControl.exe commit checkoutTest.txt checkoutTest.sh
+git checkout master
+../x64/Debug/VersionControl.exe commit checkoutTest.txt checkoutTest.sh
+
+i=0
+hash=""
+../x64/Debug/VersionControl.exe log | while read line
+do
+    if [ "$line" == "commit *" ]; then 
+        if [ $i == 1 ]; then
+            hash = echo $line | sed -ne 's/^commit //p'
+        else
+            i=i+1
+        fi
+    fi
+done
+
+../x64/Debug/VersionControl.exe checkout $hash
+cat checkoutTest.txt checkoutTest.sh
+sleep 10
+../x64/Debug/VersionControl.exe checkout HEAD
+cat checkoutTest.txt checkoutTest.sh
