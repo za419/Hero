960597665dbac66dc8e70086de71817ed69eda30
Ryan Hodin <ryan951357@gmail.com>
2017-09-18 23:50:38 -0500

    Remove checkoutTest.sh script from test files
    This generates permission errors.

--- tests/checkoutTest.sh
+++ tests/checkoutTest.sh
@@ -4,15 +4,15 @@
 
 ../x64/Debug/VersionControl.exe init
 git checkout eb2cef163bffc4f3f38663843e5a35a0a4f5744a
-../x64/Debug/VersionControl.exe commit checkoutTest.txt checkoutTest.sh
+../x64/Debug/VersionControl.exe commit checkoutTest.txt
 git checkout add-checkout
-../x64/Debug/VersionControl.exe commit checkoutTest.txt checkoutTest.sh
+../x64/Debug/VersionControl.exe commit checkoutTest.txt
 
 i=0
 hash=""
 ../x64/Debug/VersionControl.exe log | while read line
 do
-    if [ "$line" == "commit *" ]; then 
+    if [ "$line" == "commit *" ]; then
         if [ $i == 1 ]; then
             hash = echo $line | sed -ne 's/^commit //p'
         else
@@ -22,7 +22,7 @@ do
 done
 
 ../x64/Debug/VersionControl.exe checkout $hash
-cat checkoutTest.txt checkoutTest.sh
+cat checkoutTest.txt
 sleep 10
 ../x64/Debug/VersionControl.exe checkout HEAD
-cat checkoutTest.txt checkoutTest.sh
+cat checkoutTest.txt
