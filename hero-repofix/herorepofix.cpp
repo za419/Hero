cc1bf181c71f3884056ec0458fce7ca4bf9220c4
Ryan Hodin <ryan951357@gmail.com>
2018-05-14 21:43:22 -0500

    Add repofix support for upgrading to the new version

--- hero-repofix/herorepofix.cpp
+++ hero-repofix/herorepofix.cpp
@@ -1,6 +1,8 @@
 // herorepofix.cpp : Defines the entry point for the console application.
 //
 
+#include "crossplatform.h"
+#include "classes\indexmap.h"
 #include <string>
 #include <iostream>
 #include <fstream>
@@ -16,7 +18,8 @@ void usage(const char* invoke) {
 	std::cout << "    Thanks to the repository format only having breaking changes fairly rarely,\n";
 	std::cout << "      only a few versions are valid here. These are:\n";
 	std::cout << "        \"0.02.1\"\n";
-	std::cout << "        \"0.02.2 (current)\"\n";
+	std::cout << "        \"0.02.2\"\n";
+	std::cout << "        \"0.03.0 (current)\"\n";
 	std::cout << "  --heuristic\tAttempts to guess which source-version is appropriate\n";
 	std::cout << "    Any known conditions where this is guaranteed to fail shall be listed here.\n";
 	std::cout << "    This is never, however, guaranteed to succeed. If you know a past version where\n";
@@ -26,9 +29,37 @@ void usage(const char* invoke) {
 }
 
 void upgradeFrom(const std::string& source) {
-	if (source == "0.02.2") {
+	if (source == "0.03.0") {
 		// Upgrade this version to itself. Do nothing
 		exit(0);
+	}
+	else if (source == "0.02.2") {
+		// There isn't a different in the commit format or directory structures.
+		// We should, however, make sure the index stays sane
+		// And 0.03.0 entirely remade the index structure.
+		// We need to take out the index, and remake it ourselves.
+		chdir(".hero/index/"); // So that the new index is functionally equivalent to the old one
+		Indexmap imap;
+		std::vector<std::string> files;
+		if (filesInDirectory(".", files)) {
+			std::cerr << "Could not upgrade repository past 0.02.2.\n";
+			exit(1);
+		}
+
+		// Make an imap and rename files as we go
+		for (const auto& file : files) {
+			imap.add(file);
+			rename(file.c_str(), imap[file].c_str());
+		}
+
+		// Write the imap to the proper on-disk location
+		std::ofstream output("map", std::ios::out | std::ios::trunc);
+		output << imap;
+
+		// Restore our cwd
+		chdir("../../");
+		// And upgrade from the next version
+		upgradeFrom("0.03.0");
 	}
 	else if (source == "0.02.1") {
 		// The difference between 0.02.2 and 0.02.1 is simply the name of the repository directory
